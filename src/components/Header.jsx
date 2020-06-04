b20159c8a0bb777202c3f7a656a676bf511c0756
Glitch (keithk-glitch-dev) <none>
2020-06-04 19:06:59 +0000

    Before import of keithk/glitch-dev without specified path ./src/components/Header.jsx:617503/485 ./src/index.jsx:617503/396 ./src/index.css:617503/16 ./src/styles.css:617503/45

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,14 +7,13 @@ const Header = ({ roleList, filterRole, toggleFilterRole }) => (
 			<div className="md:border-r-2 justify-center" style={{ borderColor: '#5A78FF' }}>
 				<div className="pl-4 pr-8">
 					<h1 className="inline text-3xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-extrabold tracking-wide leading-snug">
-						Title
+						Personal CRM
 					</h1>
-					<h2 className="text-xl font-bold">A subtitle!</h2>
+					<h2 className="text-xl font-bold">All of my friends listed in one place!</h2>
 				</div>
 			</div>
 			<div className="pl-4 pb-8">
-				<strong>Bold leading thought, meant to make you feel.</strong> Followed by lots and lots of text
-				explaining something, idk. This is an prototype of:
+				<strong>Welcome to my prototype.</strong> This is an prototype of:
         <ul className="list-disc ml-6">
           <li>React with hot module reloading</li>
           <li>PostCSS</li>
@@ -23,6 +22,7 @@ const Header = ({ roleList, filterRole, toggleFilterRole }) => (
           <li>API Fetching (airtable-api.glitch.me: <strong>not in this project</strong>)</li>
           <li>Production bundle & deployment (right now goes to firebase, but as an example of the kind of workflow we could build, was easier than sending to s3)</li>
         </ul>
+        <p>This is the <strong>development server</strong>, which runs all of the above. Once you run <strong>npm run deploy</strong> it will bundle it all up, and deploy it to firebase.</p>
 			</div>
 		</div>
 	</div>
