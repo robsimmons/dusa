fe2624526062627e57ce57dd7a6cc334b56d85c4
Glitch (keithk-glitch-dev) <none>
2020-06-04 18:26:13 +0000

    Before import of keithk/glitch-dev without specified path

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/Glitch alumni/i);
-	expect(linkElement).toBeInTheDocument();
-});
