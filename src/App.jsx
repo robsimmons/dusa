63b93548424eb657e112e64f38faf9e03d18e42d
Glitch (keithk-glitch-dev) <none>
2020-10-16 00:39:10 +0000

    🙅🍠 Checkpoint ./src/styles.css:617503/14385 ./src/App.jsx:617503/762 ./src/illustration.svg:617503/21272 ./snowpack.config.json:617503/48 ./package.json:617503/526

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from "react";
+import './styles.css'
 
-const backgroundColor = "#9BE7D8";
-const headerColor = "#5A78FF";
+import illustration from './illustration.svg';
+
 
 export default function Home() {
   return (
-    <div>hi there!! :D </div>
+    <>
+    <div className="wrapper">
+      <div className="content">
+        <span className="title">Hello react?!</span>
+      </div>
+      <img src={illustration} className="illustration" />
+    </div>
+    <div className="navigation">
+      <div>
+        <button className="btn--remix">
+          <img
+            src="https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576"
+          />
+          Remix on Glitch
+        </button>
+      </div>
+    </div>
+  </>
   );
 }
