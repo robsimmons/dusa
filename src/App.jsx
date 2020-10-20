d769f1b51da53e7539e4c28cfc31b2c497ab730f
Glitch (keithk-glitch-dev) <none>
2020-10-20 18:03:04 +0000

    🦃🎬 Checkpoint ./src/App.jsx:617503/2518

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
 import React, { useState, useEffect } from "react";
-import './styles.css'
 
-import illustration from './illustration.svg';
+// Import and apply our CSS stylesheet
+import "./styles.css";
 
+// You can import SVG files directly
+import illustration from "./illustration.svg";
 
-const hellos = ['Hello React', 'Bonjour React']
+// Our language strings
+const hellos = ["Hello React", "Bonjour React"];
+
 
 export default function Home() {
+  const [hello, setHello] = useState(hellos[0]);
+  const handleChangeHello = () => {
+    const newHello = hellos[1];
+    setHello(newHello);
+  };
   return (
     <>
-    <div className="wrapper">
-      <div className="content">
-        <span className="title">{hellos && hellos[0]}!</span>
-        <img src={illustration} className="illustration" />
-      </div>
-    </div>
-    <div className="navigation">
-      <div>
-        <button className="btn--remix">
+      <div className="wrapper">
+        <div className="content">
+          <span className="title">{hello}!</span>
           <img
-            src="https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576"
+            src={illustration}
+            className="illustration"
+            onClick={handleChangeHello}
+            alt="Browser screen showing"
           />
-          Remix on Glitch
-        </button>
+        </div>
       </div>
-    </div>
-  </>
+      <div className="navigation">
+        <div>
+          <button className="btn--remix">
+            <img src="https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576" />
+            Remix on Glitch
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
