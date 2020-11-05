8522705fa94ed2ef7f404410913dc0c711b2c5f4
Glitch (keithk-glitch-dev) <none>
2020-11-05 18:20:52 +0000

    🎠🌇 Checkpoint ./src/styles.css:617503/66 ./src/App.jsx:617503/4681

--- src/App.jsx
+++ src/App.jsx
@@ -39,10 +39,10 @@ export default function Home() {
         <button className="btn--remix" onClick={handleChangeHello}>
           Pssst, click me
         </button>
-        <button className="btn--remix">
+        <a href="https://glitch.com/edit/#!/remix/glitch-hello-react" className="btn--remix">
           <img src="https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576" />
           Remix on Glitch
-        </button>
+        </a>
       </nav>
     </>
   );
