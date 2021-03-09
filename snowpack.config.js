a5f7ba670848f4f3e5bb351537f276ef751458cd
Glitch (keithk-glitch-dev) <none>
2021-03-09 21:57:35 +0000

    📅🍅 Checkpoint ./package.json:617503/873 ./snowpack.config.json:617503/886

---
+++ snowpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-webpack'],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
