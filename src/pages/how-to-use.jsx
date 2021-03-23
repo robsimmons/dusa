054f3be23569cff5d38f94aed7b932f5315b501e
Glitch (harsh-oasis-woodwind) <none>
2021-03-23 18:25:43 +0000

    🚩👙 Checkpoint ./src/pages/how-to-use.jsx:617503/33 ./src/styles/styles.css:617503/353 ./src/pages/next-steps.jsx:617503/370 ./src/index.jsx:617503/374 ./src/pages/home.jsx:617503/4

--- src/pages/how-to-use.jsx
+++ src/pages/how-to-use.jsx
@@ -2,9 +2,9 @@ import * as React from "react";
 
 export default function Home() {
   return (
-    <>
+    <div className="page">
       <span className="title">How to use</span>
-      <p>Some text here.</p>
-    </>
+      <p>Some text here...</p>
+    </div>
   );
 }
