681402de2842184a67932d35b7c024ce26e554b8
Glitch (glitch-hello-react) <none>
2021-03-27 02:48:42 +0000

    🏗📰 Checkpoint ./src/app.jsx:1062301/13 ./index.html:1062301/23

--- src/app.jsx
+++ src/app.jsx
@@ -24,7 +24,7 @@ export default function Home() {
         </div>
         <a
           className="btn--remix"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-eleventy"
+          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
         >
           <img src="https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576" alt="Glitch logo" />
           Remix on Glitch
