ab1895d5f1aa61dca75968bf980a642dc422a4f4
Glitch (glitch-hello-react) <none>
2021-03-29 16:34:48 +0000

    🙍🎡 Checkpoint ./src/components/seo.jsx:617503/281

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import SEO from "../seo.json";
 import { Helmet } from "react-helmet";
 
-const Seo = () => (
-  <Helmet>
+const Seo = () => {
+  return <Helmet>
     <title>{SEO.title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Glitch: React Starter</title>
@@ -14,12 +14,12 @@ const Seo = () => (
     />
     <meta name="robots" content="index,follow" />
 
-    <link rel="canonical" href="https://glitch-hello-react.glitch.me/" />
+    <link rel="canonical" href={SEO.url} />
 
     <meta property="og:title" content="Hello React!" />
     <meta property="og:type" content="article" />
 
-    <meta property="og:url" content="https://glitch-hello-react.glitch.me/" />
+    <meta property="og:url" content={SEO.url} />
     <meta
       property="og:description"
       content="A simple React site, built with Glitch. Remix it to get your own."
@@ -31,7 +31,7 @@ const Seo = () => (
 
     <meta name="twitter:card" content="summary" />
 
-    <meta name="twitter:url" content="https://glitch-hello-react.glitch.me/" />
+    <meta name="twitter:url" content={SEO.url} />
     <meta name="twitter:title" content="Hello React!" />
     <meta
       name="twitter:description"
@@ -42,6 +42,6 @@ const Seo = () => (
       content="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2Fhello-react-social.png?v=1616712748355"
     />
   </Helmet>
-);
+};
 
 export default Seo;
