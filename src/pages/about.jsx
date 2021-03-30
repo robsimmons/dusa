328e8928b71b39260b163de371fc9a586b61f658
Glitch (glitch-hello-react) <none>
2021-03-30 14:17:11 +0000

    ⛸🎻 Checkpoint ./src/pages/home.jsx:21/3 ./src/pages/about.jsx:21/1098

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,12 +4,13 @@ export default function About() {
   return (
     <div className="page">
       <span className="title">About this site</span>
+      <p>Welcome to the Glitch React starter, where you can instantly create a React site that's fully customizable.</p>
       <p>
         This page is defined in the router. It's a great spot to tell the world a few details about the new React app you built on Glitch!
       </p>
       <ul>
-        <li>🎉 Detail one</li>
-        <li>💥 Detail two</li>
+        <li>🎉 Right now, your site is <strong>live on the web</strong> 🌐 with a real URL (a secure HTTPS address!) that updates as soon as you make changes to your site or app.</li>
+        <li>💥 <strong>Add a domain</strong> to your new Glitch project! Just go to the <strong>Tools</strong> menu in the Glitch editor, and click on "Custom Domains"</li>
         <li>🌈 Detail three</li>
       </ul>
       <p>
