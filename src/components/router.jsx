bb181d2db03d9aac435a0e454361556ccb912195
Glitch (glitch-hello-react) <none>
2021-04-13 23:22:01 +0000

    🌝👵 Checkpoint ./src/components/router.jsx:617503/796

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { Link, Route } from "wouter";
+import { Switch, Route } from "wouter";
 import Home from "../pages/home";
-import About from '../pages/about'
+import About from "../pages/about";
 
 const Router = () => (
-  <>
+  <Switch>
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
-  </>
+    <Route>404, Not Found!</Route>
+  </Switch>
 );
 
 export default Router;
