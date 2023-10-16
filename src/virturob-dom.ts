f88e49448bddff5a4f88cff36e16ad0c06fe5b09
Glitch (glitch-hello-react) <none>
2023-10-16 21:14:12 +0000

    🐘🏇 Checkpoint ./src/virturob-dom.ts:578626/8229

--- src/virturob-dom.ts
+++ src/virturob-dom.ts
@@ -1,24 +1,47 @@
-type VirturobDomType = "divider" | "list" | "paragraph";
+type VirtuRobDomType = "divider" | "unordered_list" | "list_item" | "paragraph";
 
-type VirturobDomNode =
-  | string
-  | { type: "divider"; children: [] }
-  | { type: "list"; children: [] }
-  | { type: "paragraph"; children: [] };
+type VirtuRobDomNode = string | { type: VirtuRobDomType; children: [] };
 
-function parse(s: string): { nodes: VirturobDomNode[], rest: string } {
-  
+function recursiveDescentParser(s: string): { nodes: VirtuRobDomNode[]; rest: string } {
+  const result = [];
   while (true) {
-      s = s.trimStart();
-      if (s === "") return [];
-      if (s[0] === '"') {
-        const slice = s.slice(1);
-        const end = slice.indexOf('"');
-        if (end === -1) {
-          throw new Error("no matching end quote");
-        }
+    s = s.trimStart();
+    if (s[0] === '"') {
+      const slice = s.slice(1);
+      const end = slice.indexOf('"');
+      if (end === -1) {
+        throw new Error("no matching end quote");
       }
-
-      return [];
+      result.push(slice.slice(0, end));
+      s = slice.slice(end + 1);
+    } else if (s[0] === "(") {
+      s = s.slice(1).trimStart();
+      const match = s.match(/^[a-zA-Z0-9]+/);
+      if (!match) {
+        throw new Error("No dom type identifier found");
+      }
+      const domType: VirtuRobDomType | null =
+        match[0] === "divider"
+          ? "divider"
+          : match[0] === "unordered_list"
+          ? "unordered_list"
+          : match[0] === "list_item"
+          ? "list_item"
+          : match[0] === "paragraph"
+          ? "paragraph"
+          : null;
+      if (domType === null) {
+        throw new Error(`Invalid dom type '${match[0]}'`);
+      }
+      const children = recursiveDescentParser(s.slice(match[0].length));
+      s = children.rest.trimStart();
+      if (s[0] !== ")") {
+        throw new Error("Closing parenthesis expected and not found");
+      }
+      result.push({ type: domType, children: children.nodes });
+      s = s.slice(1).trimStart();
+    } else {
+      return { s}
+    }
   }
 }
