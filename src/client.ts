7a0a738a6b24c4b6981cc180c039d5aa3591f011
Rob Simmons <robsimmons@gmail.com>
2023-11-18 10:01:30 -0500

    Enable client to report facts

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { Data } from './datastructures/data';
+import { Data, expose } from './datastructures/data';
 import {
   ChoiceTree,
   ChoiceTreeNode,
@@ -6,7 +6,7 @@ import {
   pathToString,
   stepTreeRandomDFS,
 } from './engine/choiceengine';
-import { makeInitialDb } from './engine/forwardengine';
+import { Database, makeInitialDb } from './engine/forwardengine';
 import { compile } from './langauge/compile';
 import { parse } from './langauge/dusa-parser';
 import { IndexedProgram } from './langauge/indexize';
@@ -16,6 +16,13 @@ import { Issue } from './parsing/parser';
 export type { Issue, Stats };
 export type { SourcePosition, SourceLocation } from './parsing/source-location';
 
+export type Term = null | bigint | string | { name: string; args?: [Term, ...Term[]] };
+export interface Fact {
+  name: string;
+  args: Term[];
+  value: Term;
+}
+
 export class DusaError extends Error {
   issues: Issue[];
   constructor(issues: Issue[]) {
@@ -24,9 +31,20 @@ export class DusaError extends Error {
   }
 }
 
+function dataToTerm(d: Data): Term {
+  const view = expose(d);
+  if (view.type === 'triv') return null;
+  if (view.type === 'int') return view.value;
+  if (view.type === 'string') return view.value;
+  if (view.args.length === 0) return { name: view.name };
+  const args = view.args.map(dataToTerm) as [Term, ...Term[]];
+  return { name: view.name, args };
+}
+
 export class Dusa {
   private program: IndexedProgram;
   private debug: boolean;
+  private db: Database;
 
   constructor(source: string, debug = false) {
     const parsed = parse(source);
@@ -41,12 +59,15 @@ export class Dusa {
 
     this.debug = debug;
     this.program = compile(checked.decls, debug);
+    this.db = makeInitialDb(this.program);
   }
 
-  run() {
-    let tree: null | ChoiceTree = { type: 'leaf', db: makeInitialDb(this.program) };
+  run(): null | Fact[] {
+    let tree: null | ChoiceTree = { type: 'leaf', db: this.db };
     let path: [ChoiceTreeNode, Data | 'defer'][] = [];
     const stats: Stats = { cycles: 0, deadEnds: 0 };
+    let branched = false;
+
     for (;;) {
       if (tree === null) return null;
       if (this.debug) {
@@ -57,8 +78,21 @@ export class Dusa {
       tree = result.tree;
       path = result.tree === null ? path : result.path;
 
+      // If we haven't yet made any branching choices, go ahead and update
+      // the underlying database.
+      branched ||= path.length > 0 || tree === null || tree.type !== 'leaf';
+      if (!branched && tree?.type === 'leaf') {
+        this.db = tree.db;
+      }
+
       if (result.solution) {
-        return stats;
+        return result.solution.factValues
+          .entries()
+          .map(([name, args, value]): null | Fact => {
+            if (value.type === 'is not') return null;
+            return { name, args: args.map(dataToTerm), value: dataToTerm(value.value) };
+          })
+          .filter((x): x is Fact => x !== null);
       }
     }
   }
