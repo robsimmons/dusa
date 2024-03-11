8c25fcc2ce5b535ea14ff74b10188c056f045eb2
Rob Simmons <robsimmons@gmail.com>
2024-03-11 18:01:47 -0400

    Add string escapes... the fact that this didn't cause any test failures means I'm sure this has bugs.

--- README.md
+++ README.md
@@ -12,11 +12,21 @@ set programming.
 - [Source code at sourcehut](https://git.sr.ht/~robsimmons/dusa)
 - [Issue tracker at sourcehut](https://todo.sr.ht/~robsimmons/Dusa)
 
-All documentation and examples in the `docs` directory and subdirectories are
-MIT-licensed, but the Dusa implementation in JavaScript is, at present, GPL 3.0
-licensed.
-
 Dusa was initially developed by Rob Simmons and Chris Martens wile Rob attended
 [Recurse Center](https://www.recurse.com/) in Fall 2023. Recurse center is a
 great place to think about ambitious projects like this in a supportive
 environment full of interesting people!
+
+## License
+
+All documentation and examples in the `docs` directory and subdirectories are
+MIT-licensed, but the Dusa implementation in JavaScript is, at present, only licensed
+under the GNU General Public License v3.0 (see LICENSE).
+
+Dusa is presently under the GPL is to make it easy for many people to use
+the software while maintaining some ownership of software that took a
+fair amount of time and expertise to develop. If the GPL is an issue for you
+or your company but you'd like to use it, I'd be delighted to discuss
+making that possible, and if you want to sponsor me to relicense Dusa
+more permissively, I'd probably sell out pretty cheaply all things
+considered, just sayin'. --- Rob
--- src/language/dusa-tokenizer.ts
+++ src/language/dusa-tokenizer.ts
@@ -1,6 +1,6 @@
 import { BUILT_IN_MAP, BUILT_IN_PRED } from './dusa-builtins.js';
 import { Issue, ParserResponse, StreamParser } from '../parsing/parser.js';
-import { SourceLocation } from '../parsing/source-location.js';
+import { SourceLocation, SourcePosition } from '../parsing/source-location.js';
 import { StringStream } from '../parsing/string-stream.js';
 
 type StateRoot =
@@ -18,6 +18,13 @@ type StateRoot =
       hashloc: SourceLocation;
       defaults: typeof BUILT_IN_MAP;
       builtin: BUILT_IN_PRED;
+    }
+  | {
+      type: 'InString';
+      defaults: typeof BUILT_IN_MAP;
+      start: SourcePosition;
+      end: SourcePosition;
+      collected: string;
     };
 
 const punct = [
@@ -66,7 +73,6 @@ const CONST_TOKEN = /^[a-z][a-zA-Z0-9_]*$/;
 const WILDCARD_TOKEN = /^_[a-zA-Z0-9_]*$/;
 const VAR_TOKEN = /^[A-Z][a-zA-Z0-9_]*$/;
 const INT_TOKEN = /^-?(0|[1-9][0-9]*)$/;
-const STRING_CONTENTS = /^[ !#-[\]-~]*/;
 const TRIV_TOKEN = /^\(\)/;
 
 function issue(stream: StringStream, msg: string): Issue {
@@ -83,6 +89,21 @@ export const dusaTokenizer: StreamParser<ParserState, Token> = {
     let tok: string | null;
 
     if (stream.eol()) {
+      if (state.type === 'InString') {
+        console.log();
+        return {
+          state: { type: 'Normal', defaults: state.defaults },
+          tag: 'invalid',
+          issues: [
+            {
+              type: 'Issue',
+              msg: 'End of string not found at end of line',
+              loc: { start: state.start, end: state.end },
+            },
+          ],
+        };
+      }
+
       return { state };
     }
 
@@ -178,6 +199,121 @@ export const dusaTokenizer: StreamParser<ParserState, Token> = {
           tag: stream.eat('.') ? 'punctuation' : undefined,
         };
 
+      case 'InString':
+        if ((tok = stream.eat('"'))) {
+          return {
+            state: { type: 'Normal', defaults: state.defaults },
+            tag: 'string',
+            tree: {
+              type: 'string',
+              value: state.collected,
+              loc: { start: state.start, end: stream.matchedLocation().end },
+            },
+          };
+        }
+
+        if ((tok = stream.eat(/^[^"\n\r\\]+/))) {
+          return {
+            state: {
+              ...state,
+              collected: state.collected + tok,
+              end: stream.matchedLocation().end,
+            },
+            tag: 'string',
+          };
+        }
+
+        if (stream.eat('\\')) {
+          if (
+            (tok = stream.eat(/^([0bfnrtv'"\\]|x[0-9a-fA-F][0-9a-fA-F]|u\{[0-9a-fA-F]{1,6}\})/))
+          ) {
+            switch (tok[0]) {
+              case '0':
+                tok = '\0';
+                break;
+              case 'b':
+                tok = '\b';
+                break;
+              case 'f':
+                tok = '\f';
+                break;
+              case 'n':
+                tok = '\n';
+                break;
+              case 'r':
+                tok = '\r';
+                break;
+              case 't':
+                tok = '\t';
+                break;
+              case 'v':
+                tok = '\v';
+                break;
+              case "'":
+                tok = "'";
+                break;
+              case '"':
+                tok = '"';
+                break;
+              case 'x':
+                tok = String.fromCharCode(parseInt(tok.slice(1), 16));
+                break;
+              default:
+                // case 'u'
+                const charCode = parseInt(tok.slice(2, tok.length - 1), 16);
+                if (charCode > 0x10ffff) {
+                  return {
+                    state,
+                    issues: [
+                      {
+                        type: 'Issue',
+                        msg: `Bad Unicode code point ${tok}`,
+                        loc: stream.matchedLocation(),
+                      },
+                    ],
+                  };
+                } else {
+                  tok = String.fromCharCode(charCode);
+                  break;
+                }
+            }
+            return {
+              state: {
+                ...state,
+                collected: state.collected + tok,
+                end: stream.matchedLocation().end,
+              },
+              tag: 'escape',
+            };
+          }
+          if ((tok = stream.eat(/^./))) {
+            return {
+              state,
+              tag: 'invalid',
+              issues: [
+                {
+                  type: 'Issue',
+                  msg: `Invalid escape sequence \\${tok}`,
+                  loc: stream.matchedLocation(),
+                },
+              ],
+            };
+          }
+          return {
+            state: { type: 'Normal', defaults: state.defaults },
+            tag: 'invalid',
+            issues: [
+              {
+                type: 'Issue',
+                msg: 'Backslash not supported at end of line',
+                loc: stream.matchedLocation(),
+              },
+            ],
+          };
+        }
+
+        throw new Error('Expected-to-be-unimpossible state in string parsing reached');
+
       case 'Normal':
         if ((tok = stream.eat('#'))) {
           tok = stream.eat(META_ID_TOKEN) ?? stream.eat(META_NUM_TOKEN);
@@ -199,32 +335,15 @@ export const dusaTokenizer: StreamParser<ParserState, Token> = {
         }
 
         if (stream.eat('"')) {
-          const value = stream.eat(STRING_CONTENTS)!;
-          if (stream.eat('"')) {
-            return {
-              state,
-              tag: 'string',
-              tree: { type: 'string', value, loc: stream.matchedLocation() },
-            };
-          }
-
-          if ((tok = stream.eat(/^[^"]+"?/))) {
-            return {
-              state,
-              tag: 'invalid',
-              issues: [
-                issue(
-                  stream,
-                  `Expected double-quote to end string, found unexpected character '${tok[0]}' instead`,
-                ),
-              ],
-            };
-          }
-
           return {
-            state,
-            tag: 'invalid',
-            issues: [issue(stream, 'End of string not found at end of line')],
+            state: {
+              ...state,
+              type: 'InString',
+              start: stream.matchedLocation().start,
+              end: stream.matchedLocation().end,
+              collected: '',
+            },
+            tag: 'string',
           };
         }
 
@@ -309,7 +428,21 @@ export const dusaTokenizer: StreamParser<ParserState, Token> = {
         };
     }
   },
-  handleEof: (): ParserResponse<ParserState, Token> | null => {
+  handleEof: (state): ParserResponse<ParserState, Token> | null => {
+    if (state.type === 'InString') {
+      console.log();
+      return {
+        state: { type: 'Normal', defaults: state.defaults },
+        tag: 'invalid',
+        issues: [
+          {
+            type: 'Issue',
+            msg: 'End of string not found at end of input',
+            loc: { start: state.start, end: state.end },
+          },
+        ],
+      };
+    }
     return null;
   },
 };
