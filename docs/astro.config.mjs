23ba6b9ad1fd885e5999ec8f10a7bda1a6f97da4
Rob Simmons <robsimmons@gmail.com>
2024-05-16 13:22:59 -0400

    Prettier

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dusa.rocks",
+  site: 'https://dusa.rocks',
   locale: 'https://google.com/',
   integrations: [
     starlight({
