be3b5a463ed6b3ef79b8546a3f6ad3df1b7eea88
Rob Simmons <robsimmons@gmail.com>
2024-05-19 14:20:00 -0400

    Revert yml but add exclusion/inclusion to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: { provider: 'istanbul' },
+    coverage: {
+      provider: 'istanbul',
+      include: ['src'],
+      exclude: ['src/web'],
+    },
   },
 });
